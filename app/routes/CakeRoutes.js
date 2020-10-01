3c70b3cca70b823f20ad88e37a92eac7a78784ba
sumitkumar <sumit@binarynumbers.io>
2020-10-01 09:09:39 +0530

    Get all cakes and single cake by id routes.

--- app/routes/CakeRoutes.js
+++ app/routes/CakeRoutes.js
@@ -4,8 +4,14 @@ const router = express.Router();
 const CakeConteroller = require('@/controller/CakeController/CakeController');
 const { isAuthorizedUser } = require('@/app/middleware/auth');
 const roles = require('@/app/helpers/permission');
-
+//Add new cake route
 router.route('/add-cake')
   .post(isAuthorizedUser([roles.admin, roles.super_admin]), CakeConteroller.AddCake)
+//Get all the cakes
+router.route('/get-cakes')
+  .get(isAuthorizedUser(), CakeConteroller.GetCakes)
+//Get the cake detail from cake id
+router.route('/get-cake/:cake_id')
+  .get(isAuthorizedUser(), CakeConteroller.GetCakeByCakeId)
 
 module.exports = router
